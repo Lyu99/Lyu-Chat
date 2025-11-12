c9689e6cc4879cb6f967e46c7ac3389bccba1403
Lyu <lyuping99@163.com>
2025-11-12 14:56:55 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center justify-between">
+    <div class="bg-gray-300 text-gray-700">
+      side
+    </div>
+    <div class="bg-green-300 text-green-700">
+      content
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+
+</style>
